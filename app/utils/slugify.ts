44ef478a20cd263f80164b934960719c59c1421f
Malik Browne <malik.browne3@gmail.com>
2025-02-07 20:57:54 -0500

    fix eslint

--- app/utils/slugify.ts
+++ app/utils/slugify.ts
@@ -2,8 +2,8 @@ export function slugify(text: string): string {
   return text
     .toLowerCase()
     .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
-    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/[^\w-]+/g, '') // Remove all non-word chars
+    .replace(/-{2,}/g, '-') // Replace multiple - with single -
     .replace(/^-+/, '') // Trim - from start of text
     .replace(/-+$/, ''); // Trim - from end of text
 }
--- app/utils/supabase/client.ts
+++ app/utils/supabase/client.ts
@@ -5,7 +5,7 @@ function createClient() {
   // Create a supabase client on the browser with project's credentials
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
 }
 
